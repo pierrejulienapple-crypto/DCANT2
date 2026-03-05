d4edaef7934cd967c7621a603c1f2492bfe172cc
pierrejulienapple-crypto <pierrejulienapple@gmail.com>
2026-03-05 17:45:14 +0100

    Delete anthropic-key.js

--- anthropic-key.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req) {
-  return new Response(
-    JSON.stringify({ key: process.env.ANTHROPIC_KEY || '' }),
-    { headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } }
-  );
-}
